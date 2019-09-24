fe3e8231566a7179d18abf3040272798ffe53d76
Juan Braian Hernandez Morani <55467194+ElChetoMDQ97@users.noreply.github.com>
2019-09-24 20:15:21 -0300

    Add files via upload

---
+++ encuesta.php
@@ -0,0 +1,32 @@
+<html>
+ <head>
+ <title>
+        PlayStation 1
+    </title>
+
+    <link rel="stylesheet" type="text/css" href="Estilo.css">
+
+    <meta charset="UTF-8">
+ </head>
+ <body background="psx.jpg">
+ <h1 class="Estilo3"> PlayStation (プレイステーション Pureisutēshon, oficialmente abreviada como PS1) </h1>
+
+<h2 class="Estilo"> Video-consola de sobremesa </h2>
+
+<p class="Estilo2" style="background: darkgrey"> La PlayStation fue lanzada a la venta el 3 de diciembre de 1994 en Japón, el 9 de septiembre de 1995 en EE. UU. y el 29 de septiembre de 1995 en Europa. Todo empezó con un contrato roto con Nintendo a finales de la década de 1980. Nintendo acordó
+    con Sony, a finales de los 80, desarrollar para su exitosa Super Nintendo un apéndice para incorporar juegos en disco, además del tradicional cartucho. </p>
+
+ Hola <?php echo htmlspecialchars($_POST['nombre']); ?>. <br>
+
+ <br> <a href="http://google.com.ar" target="_blank"> click aqui para ir a google </a>
+
+
+            <br> <a href="https://es.wikipedia.org/wiki/PlayStation" target="_blank"> Informacion adicional </a>
+
+
+            <br> <a href="https://github.com" target="_blank"> GitHub </a>
+
+
+            <br> <img src="playstation.jpg">
+ </body>
+</html>
